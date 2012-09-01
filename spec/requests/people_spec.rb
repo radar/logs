682101e5b24959e386bd087b9a4e025e0d087f5b
Ryan Bigg <radarlistener@gmail.com>
2012-09-01 19:07:12 +0100

    Ensure hidden messages are, indeed, hidden

--- spec/requests/people_spec.rb
+++ spec/requests/people_spec.rb
@@ -2,14 +2,13 @@ require 'spec_helper'
 
 describe 'people' do
   let!(:channel) { FactoryGirl.create(:channel) }
-  before do
-    person = Person.create!(:nick => "Radar")
+  let!(:person) { Person.create!(:nick => "Radar") }
+
+  it "can click on a person's name from the messages page" do
     other = Person.create!(:nick => "Other")
     channel.messages.create(:person => person, :text => "Testing")
     channel.messages.create(:person => other, :text => "Other")
-  end
 
-  it "can click on a person's name from the messages page" do
     visit channel_path(channel)
     within("#messages") do
       click_link "Radar"
@@ -19,5 +18,13 @@ describe 'people' do
       page.should have_content("Testing")
       page.should_not have_content("Other")
     end
+  end
+  
+  it "should not show hidden messages" do
+    channel = FactoryGirl.create(:channel, :hidden => true)
+    channel.messages.create(:person => person, :text => "Crouching Tiger")
+
+    visit person_path(person)
+    all("#messages div").count.should == 0
   end
 end
