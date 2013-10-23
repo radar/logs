994634060ffba2b215e5d25aa0f531160c6e5787
Ryan Bigg <radarlistener@gmail.com>
2013-10-23 11:38:56 +1100

    Move calendar to activity

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Logs::Application.routes.draw do
 
   get '/tips', :to => "tips#index"
   get '/p/:nick', :to => "people#show", :as => :person
-  get '/p/:nick/calendar', :to => "people#calendar"
+  get '/p/:nick/activity', :to => "people#activity", :as => :person_activity
   get ':name', :to => "channels#show", :as => :channel
 end
