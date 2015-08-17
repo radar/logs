fedfe6593a611bfad6656d956e3263ec07e190fc
Ryan Bigg <git@ryanbigg.com>
2015-08-17 10:29:20 +1000

    Cache channels/show

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -8,9 +8,6 @@ class ChannelsController < ApplicationController
     @channel = Channel.where("NAME ILIKE ?", params[:name]).first
     @date = params[:date] ? Time.zone.parse(params[:date]).to_date : Time.zone.now.to_date
     @messages = @channel.messages.by_day(@date).includes(:person, :channel)
-
-    if params[:date] && Date.parse(params[:date]) != Date.today
-      expires_in 5.years
-    end
+    fresh_when last_modified: @messages.last.created_at, etag: @messages.last
   end
 end
